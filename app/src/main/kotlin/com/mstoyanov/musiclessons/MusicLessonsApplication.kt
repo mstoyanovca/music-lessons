d3f975b1e2503536af577a3062d85934e702378d
mstoyanovca <mstoyanovca@gmail.com>
2020-11-28 00:33:26 -0500

    fixed the DB migration

--- app/src/main/kotlin/com/mstoyanov/musiclessons/MusicLessonsApplication.kt
+++ app/src/main/kotlin/com/mstoyanov/musiclessons/MusicLessonsApplication.kt
@@ -19,23 +19,29 @@ class MusicLessonsApplication : Application() {
 
         private val MIGRATION_3_4 = object : Migration(3, 4) {
             override fun migrate(database: SupportSQLiteDatabase) {
-                database.execSQL("CREATE TABLE IF NOT EXISTS student_4 (student_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, first_name TEXT NOT NULL, last_name TEXT NOT NULL, email TEXT NOT NULL, notes TEXT NOT NULL);")
-                database.execSQL("CREATE TABLE IF NOT EXISTS phone_number_4 (phone_number_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, number TEXT NOT NULL, type TEXT NOT NULL, student_owner_id INTEGER NOT NULL, FOREIGN KEY(student_owner_id) REFERENCES student(student_id) ON UPDATE NO ACTION ON DELETE CASCADE);")
-                database.execSQL("CREATE INDEX IF NOT EXISTS index_phone_number_4_student_owner_id ON phone_number_4(student_owner_id);")
-                database.execSQL("CREATE TABLE IF NOT EXISTS lesson_4 (lesson_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, weekday TEXT NOT NULL, time_from TEXT NOT NULL, time_to TEXT NOT NULL, student_owner_id INTEGER NOT NULL, FOREIGN KEY(student_owner_id) REFERENCES student(student_id) ON UPDATE NO ACTION ON DELETE CASCADE );")
-                database.execSQL("CREATE INDEX IF NOT EXISTS index_lesson_4_student_owner_id ON lesson_4(student_owner_id);")
+                database.execSQL("begin transaction;")
+
+                database.execSQL("create table if not exists student_4 (student_id integer primary key autoincrement not null, first_name text not null, last_name text not null, email text not null, notes text not null);")
+                database.execSQL("create table if not exists phone_number_4 (phone_number_id integer primary key autoincrement not null, number text not null, type text not null, student_owner_id integer not null, foreign key(student_owner_id) references student(student_id) on update no action on delete cascade);")
+                database.execSQL("create table if not exists lesson_4 (lesson_id integer primary key autoincrement not null, weekday text not null, time_from text not null, time_to text not null, student_owner_id integer not null, foreign key(student_owner_id) references student(student_id) on update no action on delete cascade);")
 
                 database.execSQL("insert into student_4 (student_id, first_name, last_name, email, notes) select s_id, first_name, last_name, email, notes from student;")
                 database.execSQL("insert into phone_number_4 (phone_number_id, number, type, student_owner_id) select phone_number_id, number, type, student_id from phone_number;")
-                database.execSQL("insert into lesson_4 (lesson_id, weekday, time_from, time_to, student_owner_id) select lesson_id, weekday, time_from, time_to, student_id from lesson;")
+                database.execSQL("insert into lesson_4 (lesson_id, weekday, time_from, time_to, student_owner_id) select lesson_id, weekday, strftime('%H:%M', time_from/1000, 'unixepoch'), strftime('%H:%M', time_to/1000, 'unixepoch'), student_id from lesson;")
 
                 database.execSQL("drop table if exists student;")
                 database.execSQL("drop table if exists phone_number;")
+                database.execSQL("drop index if exists index_phone_number_student_id;")
                 database.execSQL("drop table if exists lesson;")
+                database.execSQL("drop index if exists index_lesson_student_id;")
 
                 database.execSQL("alter table student_4 rename to student;")
                 database.execSQL("alter table phone_number_4 rename to phone_number;")
+                database.execSQL("create index if not exists index_phone_number_student_owner_id on phone_number(student_owner_id);")
                 database.execSQL("alter table lesson_4 rename to lesson;")
+                database.execSQL("create index if not exists index_lesson_student_owner_id on lesson(student_owner_id);")
+
+                database.execSQL("commit;")
             }
         }
     }
