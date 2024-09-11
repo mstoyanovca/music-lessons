d315429a8f03bd085eec8084127ed7d75ef38013
mstoyanovca <mstoyanovca@gmail.com>
2024-09-11 11:23:47 -0400

    bugfix

--- app/src/main/kotlin/com/mstoyanov/musiclessons/model/PhoneNumberTypeConverter.kt
+++ app/src/main/kotlin/com/mstoyanov/musiclessons/model/PhoneNumberTypeConverter.kt
@@ -3,6 +3,18 @@ package com.mstoyanov.musiclessons.model
 import androidx.room.TypeConverter
 
 class PhoneNumberTypeConverter {
+    // do not delete this method, IntelliJ doesn't see it being used, but it is:
+    @TypeConverter
+    fun toPhoneNumberType(value: String): PhoneNumberType? {
+        return when (value) {
+            "Home" -> PhoneNumberType.HOME
+            "Cell" -> PhoneNumberType.CELL
+            "Work" -> PhoneNumberType.WORK
+            "Other" -> PhoneNumberType.OTHER
+            else -> null
+        }
+    }
+
     @TypeConverter
     fun toString(phoneNumberType: PhoneNumberType): String {
         return phoneNumberType.displayValue()
