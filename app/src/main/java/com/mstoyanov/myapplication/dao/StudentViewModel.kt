15de4b1863e205a771989236bf1cb864c3991c33
mstoyanovca <mstoyanovca@gmail.com>
2026-05-18 14:06:50 -0400

    craeted StudentViewModel.kt

--- app/src/main/java/com/mstoyanov/myapplication/dao/StudentViewModel.kt
+++ app/src/main/java/com/mstoyanov/myapplication/dao/StudentViewModel.kt
@@ -1,22 +1,36 @@
 package com.mstoyanov.myapplication.dao
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.mstoyanov.myapplication.entity.StudentWithPhoneNumbers
+import com.mstoyanov.myapplication.entity.Student
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
-class StudentViewModel(private val studentDao: StudentDao) : ViewModel() {
-    private val _students = MutableStateFlow<List<StudentWithPhoneNumbers>>(emptyList())
-    val students: StateFlow<List<StudentWithPhoneNumbers>> = _students.asStateFlow()
+class StudentViewModel(private val studentDao: StudentDao, private val savedStateHandle: SavedStateHandle) : ViewModel() {
+    private val _students = MutableStateFlow<List<Student>>(emptyList())
+    val students: StateFlow<List<Student>> = _students.asStateFlow()
 
     fun findAll() {
         viewModelScope.launch {
-            studentDao.findAllWithPhoneNumbers().collect { students ->
+            studentDao.findAll().collect { students ->
                 _students.value = students
             }
         }
     }
+
+    /*companion object {
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val savedStateHandle = createSavedStateHandle()
+                val x = (this[APPLICATION_KEY] as MusicLessonsApplication).db
+                StudentViewModel(
+                    studentDao = (this[APPLICATION_KEY] as MusicLessonsApplication).db
+                    savedStateHandle = savedStateHandle
+                )
+            }
+        }
+    }*/
 }
