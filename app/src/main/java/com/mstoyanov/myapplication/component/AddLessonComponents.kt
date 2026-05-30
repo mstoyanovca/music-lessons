415b6465258d67aab4a0dc86d019f109985b5a24
mstoyanovca <mstoyanovca@gmail.com>
2026-05-29 23:33:06 -0400

    refactoring

--- app/src/main/java/com/mstoyanov/myapplication/component/AddLessonComponents.kt
+++ app/src/main/java/com/mstoyanov/myapplication/component/AddLessonComponents.kt
@@ -4,18 +4,13 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.plus
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.input.TextFieldLineLimits
@@ -23,10 +18,8 @@ import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.foundation.text.input.setTextAndPlaceCursorAtEnd
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.AccessTime
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Save
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuAnchorType
@@ -38,22 +31,16 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TimePicker
-import androidx.compose.material3.TimePickerState
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
@@ -67,7 +54,6 @@ import com.mstoyanov.myapplication.entity.Student
 import com.mstoyanov.myapplication.entity.Weekday
 import com.mstoyanov.myapplication.function.weekdayFromPage
 import java.time.LocalTime
-import java.time.format.DateTimeFormatter
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
@@ -132,24 +118,6 @@ private fun TopAppBarImpl(navigateBack: () -> Unit) {
                 )
             }
         },
-    )
-}
-
-@Composable
-private fun WeekdayContent(weekday: Weekday?) {
-    Text(
-        modifier = Modifier
-            .background(
-                MaterialTheme.colorScheme.primaryContainer,
-                shape = RoundedCornerShape(8.dp)
-            )
-            .height(48.dp)
-            .wrapContentHeight(align = Alignment.CenterVertically)
-            .padding(horizontal = 16.dp)
-            .fillMaxWidth(),
-        text = weekday!!.value,
-        style = MaterialTheme.typography.titleLarge,
-        color = MaterialTheme.colorScheme.onPrimaryContainer
     )
 }
 
@@ -232,151 +200,4 @@ private fun Modifier.crop(vertical: Dp): Modifier = this.layout { measurable, co
     ) {
         placeable.placeRelative(0, -vertical.toPx().toInt())
     }
-}
-
-@Composable
-@OptIn(ExperimentalMaterial3Api::class)
-private fun TimePicker(timeFrom: LocalTime, timeTo: LocalTime, onTimeFromSelect: (LocalTime) -> Unit, onTimeToSelect: (LocalTime) -> Unit) {
-    var timeFromPickerState = rememberTimePickerState(
-        is24Hour = true,
-        initialHour = timeFrom.hour,
-        initialMinute = timeFrom.minute
-    )
-    var timeToPickerState = rememberTimePickerState(
-        is24Hour = true,
-        initialHour = timeTo.hour,
-        initialMinute = timeTo.minute
-    )
-    var selectedTimeFrom by rememberSaveable { mutableStateOf(formattedTime(timeFrom.hour, timeFrom.minute)) }
-    var selectedTimeTo by rememberSaveable { mutableStateOf(formattedTime(timeTo.hour, timeTo.minute)) }
-    var showTimeFromPicker by rememberSaveable { mutableStateOf(false) }
-    var showTimeToPicker by rememberSaveable { mutableStateOf(false) }
-
-    Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
-        Row(
-            modifier = Modifier
-                .height(48.dp)
-                .clip(RoundedCornerShape(8.dp))
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .fillMaxWidth()
-                .clickable(onClick = { showTimeFromPicker = true }),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Icon(
-                modifier = Modifier.padding(start = 12.dp),
-                imageVector = Icons.Default.AccessTime,
-                contentDescription = null,
-                tint = Color.Blue
-            )
-            Spacer(Modifier.width(16.dp))
-            Text(
-                text = "Time From: $selectedTimeFrom",
-                style = MaterialTheme.typography.bodyLarge,
-                color = MaterialTheme.colorScheme.onPrimaryContainer
-            )
-        }
-        Row(
-            modifier = Modifier
-                .height(48.dp)
-                .clip(RoundedCornerShape(8.dp))
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .fillMaxWidth()
-                .clickable(onClick = { showTimeToPicker = true }),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Icon(
-                modifier = Modifier.padding(start = 12.dp),
-                imageVector = Icons.Default.AccessTime,
-                contentDescription = null,
-                tint = Color.Blue
-            )
-            Spacer(Modifier.width(16.dp))
-            Text(
-                text = "Time To: $selectedTimeTo",
-                style = MaterialTheme.typography.bodyLarge,
-                color = MaterialTheme.colorScheme.onPrimaryContainer
-            )
-        }
-    }
-
-    if (showTimeFromPicker) {
-        TimePickerDialog(timeFromPickerState, onConfirm = {
-            timeFromPickerState = it
-
-            val timeFrom = LocalTime.of(timeFromPickerState.hour, timeFromPickerState.minute)
-            var timeTo = LocalTime.of(timeToPickerState.hour, timeToPickerState.minute)
-            if (timeFrom.isAfter(timeTo)) {
-                timeTo = timeFrom.plusMinutes(30)
-                timeToPickerState.hour = timeTo.hour
-                timeToPickerState.minute = timeTo.minute
-                selectedTimeTo = formattedTime(timeToPickerState.hour, timeToPickerState.minute)
-                onTimeToSelect(timeTo)
-            }
-
-            selectedTimeFrom = formattedTime(timeFromPickerState.hour, timeFromPickerState.minute)
-            onTimeFromSelect(timeFrom)
-            showTimeFromPicker = false
-        }, onDismiss = {
-            showTimeFromPicker = false
-        })
-    }
-
-    if (showTimeToPicker) {
-        TimePickerDialog(timeToPickerState, onConfirm = {
-            timeToPickerState = it
-
-            var timeFrom = LocalTime.of(timeFromPickerState.hour, timeFromPickerState.minute)
-            val timeTo = LocalTime.of(timeToPickerState.hour, timeToPickerState.minute)
-            if (timeTo.isBefore(timeFrom)) {
-                timeFrom = timeTo.minusMinutes(30)
-                timeFromPickerState.hour = timeFrom.hour
-                timeFromPickerState.minute = timeFrom.minute
-                selectedTimeFrom = formattedTime(timeFromPickerState.hour, timeFromPickerState.minute)
-                onTimeFromSelect(timeFrom)
-            }
-
-            selectedTimeTo = formattedTime(timeToPickerState.hour, timeToPickerState.minute)
-            onTimeToSelect(timeTo)
-            showTimeToPicker = false
-        }, onDismiss = {
-            showTimeToPicker = false
-        })
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-private fun TimePickerDialog(
-    timePickerState: TimePickerState,
-    onConfirm: (TimePickerState) -> Unit,
-    onDismiss: () -> Unit,
-) {
-    AlertDialog(
-        onDismissRequest = onDismiss,
-        dismissButton = {
-            TextButton(onClick = { onDismiss() }) {
-                Text(
-                    text = "Cancel",
-                    style = MaterialTheme.typography.bodyLarge,
-                    color = MaterialTheme.colorScheme.error
-                )
-            }
-        },
-        confirmButton = {
-            TextButton(onClick = { onConfirm(timePickerState) }) {
-                Text(
-                    text = "OK",
-                    style = MaterialTheme.typography.bodyLarge,
-                    color = Color(0xFF1B5E20)
-                )
-            }
-        },
-        text = { TimePicker(state = timePickerState) }
-    )
-}
-
-private fun formattedTime(hour: Int, minute: Int): String {
-    return LocalTime
-        .of(hour, minute)
-        .format(DateTimeFormatter.ofPattern("HH:mm"))
 }
--- app/src/main/java/com/mstoyanov/myapplication/component/EditLessonComponents.kt
+++ app/src/main/java/com/mstoyanov/myapplication/component/EditLessonComponents.kt
@@ -2,10 +2,8 @@ package com.mstoyanov.myapplication.component
 
 import android.os.Bundle
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -15,15 +13,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.plus
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.AccessTime
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Save
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -32,12 +27,8 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.TimePicker
-import androidx.compose.material3.TimePickerState
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -64,9 +55,6 @@ import androidx.lifecycle.viewmodel.viewModelFactory
 import androidx.savedstate.SavedStateRegistryOwner
 import com.mstoyanov.myapplication.dao.LessonViewModel
 import com.mstoyanov.myapplication.entity.Lesson
-import com.mstoyanov.myapplication.entity.Weekday
-import java.time.LocalTime
-import java.time.format.DateTimeFormatter
 
 @Composable
 fun EditLessonProgressIndicator(lessonId: Long, navigateBack: () -> Unit) {
@@ -96,7 +84,10 @@ fun EditLessonProgressIndicator(lessonId: Long, navigateBack: () -> Unit) {
         }
 
         else -> {
-            EditLesson(lesson, navigateBack)
+            EditLesson(
+                lesson,
+                navigateBack,
+                onUpdateLessonClick = { lessonViewModel.update(it) })
         }
     }
 }
@@ -106,8 +97,7 @@ fun EditLessonProgressIndicator(lessonId: Long, navigateBack: () -> Unit) {
 fun EditLesson(
     lesson: Lesson,
     navigateBack: () -> Unit,
-    // TODO:
-    lessonViewModel: LessonViewModel = viewModel()
+    onUpdateLessonClick: (Lesson) -> Unit,
 ) {
     var timeFrom by rememberSaveable { mutableStateOf(lesson.timeFrom) }
     var timeTo by rememberSaveable { mutableStateOf(lesson.timeTo) }
@@ -117,7 +107,7 @@ fun EditLesson(
         floatingActionButton = {
             FloatingActionButton(
                 onClick = {
-                    lessonViewModel.update(lesson.copy(timeFrom = timeFrom, timeTo = timeTo))
+                    onUpdateLessonClick(lesson.copy(timeFrom = timeFrom, timeTo = timeTo))
                     navigateBack()
                 }
             ) {
@@ -157,24 +147,6 @@ private fun TopAppBarImpl(navigateBack: () -> Unit) {
 }
 
 @Composable
-private fun WeekdayContent(weekday: Weekday?) {
-    Text(
-        modifier = Modifier
-            .background(
-                color = MaterialTheme.colorScheme.primaryContainer,
-                shape = RoundedCornerShape(8.dp)
-            )
-            .height(48.dp)
-            .wrapContentHeight(align = Alignment.CenterVertically)
-            .padding(horizontal = 16.dp)
-            .fillMaxWidth(),
-        text = weekday!!.value,
-        style = MaterialTheme.typography.titleLarge,
-        color = MaterialTheme.colorScheme.onPrimaryContainer
-    )
-}
-
-@Composable
 private fun StudentContent(studentName: String) {
     Row(
         modifier = Modifier
@@ -199,156 +171,4 @@ private fun StudentContent(studentName: String) {
             overflow = TextOverflow.Ellipsis
         )
     }
-}
-
-@Composable
-@OptIn(ExperimentalMaterial3Api::class)
-private fun TimePicker(
-    timeFrom: LocalTime,
-    timeTo: LocalTime,
-    onTimeFromSelect: (LocalTime) -> Unit,
-    onTimeToSelect: (LocalTime) -> Unit
-) {
-    var timeFromPickerState = rememberTimePickerState(
-        is24Hour = true,
-        initialHour = timeFrom.hour,
-        initialMinute = timeFrom.minute
-    )
-    var timeToPickerState = rememberTimePickerState(
-        is24Hour = true,
-        initialHour = timeTo.hour,
-        initialMinute = timeTo.minute
-    )
-    var selectedTimeFrom by rememberSaveable { mutableStateOf(formattedTime(timeFrom.hour, timeFrom.minute)) }
-    var selectedTimeTo by rememberSaveable { mutableStateOf(formattedTime(timeTo.hour, timeTo.minute)) }
-    var showTimeFromPicker by rememberSaveable { mutableStateOf(false) }
-    var showTimeToPicker by rememberSaveable { mutableStateOf(false) }
-
-    Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
-        Row(
-            modifier = Modifier
-                .height(48.dp)
-                .clip(RoundedCornerShape(8.dp))
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .fillMaxWidth()
-                .clickable(onClick = { showTimeFromPicker = true }),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Icon(
-                modifier = Modifier.padding(start = 12.dp),
-                imageVector = Icons.Default.AccessTime,
-                contentDescription = null,
-                tint = Color.Blue
-            )
-            Spacer(Modifier.width(16.dp))
-            Text(
-                text = "Time From: $selectedTimeFrom",
-                style = MaterialTheme.typography.bodyLarge,
-                color = MaterialTheme.colorScheme.onPrimaryContainer
-            )
-        }
-        Row(
-            modifier = Modifier
-                .height(48.dp)
-                .clip(RoundedCornerShape(8.dp))
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .fillMaxWidth()
-                .clickable(onClick = { showTimeToPicker = true }),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Icon(
-                modifier = Modifier.padding(start = 12.dp),
-                imageVector = Icons.Default.AccessTime,
-                contentDescription = null,
-                tint = Color.Blue
-            )
-            Spacer(Modifier.width(16.dp))
-            Text(
-                text = "Time To: $selectedTimeTo",
-                style = MaterialTheme.typography.bodyLarge,
-                color = MaterialTheme.colorScheme.onPrimaryContainer
-            )
-        }
-    }
-
-    if (showTimeFromPicker) {
-        TimePickerDialog(timeFromPickerState, onConfirm = {
-            timeFromPickerState = it
-
-            val timeFrom = LocalTime.of(timeFromPickerState.hour, timeFromPickerState.minute)
-            var timeTo = LocalTime.of(timeToPickerState.hour, timeToPickerState.minute)
-            if (timeFrom.isAfter(timeTo)) {
-                timeTo = timeFrom.plusMinutes(30)
-                timeToPickerState.hour = timeTo.hour
-                timeToPickerState.minute = timeTo.minute
-                selectedTimeTo = formattedTime(timeToPickerState.hour, timeToPickerState.minute)
-                onTimeToSelect(timeTo)
-            }
-
-            selectedTimeFrom = formattedTime(timeFromPickerState.hour, timeFromPickerState.minute)
-            onTimeFromSelect(timeFrom)
-            showTimeFromPicker = false
-        }, onDismiss = {
-            showTimeFromPicker = false
-        })
-    }
-
-    if (showTimeToPicker) {
-        TimePickerDialog(timeToPickerState, onConfirm = {
-            timeToPickerState = it
-
-            var timeFrom = LocalTime.of(timeFromPickerState.hour, timeFromPickerState.minute)
-            val timeTo = LocalTime.of(timeToPickerState.hour, timeToPickerState.minute)
-            if (timeTo.isBefore(timeFrom)) {
-                timeFrom = timeTo.minusMinutes(30)
-                timeFromPickerState.hour = timeFrom.hour
-                timeFromPickerState.minute = timeFrom.minute
-                selectedTimeFrom = formattedTime(timeFromPickerState.hour, timeFromPickerState.minute)
-                onTimeFromSelect(timeFrom)
-            }
-
-            selectedTimeTo = formattedTime(timeToPickerState.hour, timeToPickerState.minute)
-            onTimeToSelect(timeTo)
-            showTimeToPicker = false
-        }, onDismiss = {
-            showTimeToPicker = false
-        })
-    }
-}
-
-@Composable
-@OptIn(ExperimentalMaterial3Api::class)
-private fun TimePickerDialog(
-    timePickerState: TimePickerState,
-    onConfirm: (TimePickerState) -> Unit,
-    onDismiss: () -> Unit,
-) {
-    AlertDialog(
-        onDismissRequest = onDismiss,
-        dismissButton = {
-            TextButton(onClick = { onDismiss() }) {
-                Text(
-                    text = "Cancel",
-                    style = MaterialTheme.typography.bodyLarge,
-                    color = MaterialTheme.colorScheme.error
-                )
-            }
-        },
-        confirmButton = {
-            TextButton(onClick = { onConfirm(timePickerState) }) {
-                Text(
-                    text = "OK",
-                    style = MaterialTheme.typography.bodyLarge,
-                    color = Color(0xFF1B5E20)
-                )
-            }
-        },
-        text = { TimePicker(state = timePickerState) }
-    )
-}
-
-private fun formattedTime(hour: Int, minute: Int): String {
-    return LocalTime
-        .of(hour, minute)
-        .format(DateTimeFormatter.ofPattern("HH:mm"))
 }
