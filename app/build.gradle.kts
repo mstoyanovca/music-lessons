23e26b400121ec531b0437d536c44ffe76aeb29d
mstoyanovca <mstoyanovca@gmail.com>
2026-05-30 18:10:06 -0400

    cleaning dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,9 +64,7 @@ dependencies {
 
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
 }
