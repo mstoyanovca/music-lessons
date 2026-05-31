11aef7a05d5fa1630f507d556c7fefaeeb260aa1
mstoyanovca <mstoyanovca@gmail.com>
2026-05-30 20:16:34 -0400

    adding DB migration to version 6

--- app/src/main/java/com/mstoyanov/myapplication/dao/MusicLessonsDatabase.kt
+++ app/src/main/java/com/mstoyanov/myapplication/dao/MusicLessonsDatabase.kt
@@ -1,13 +1,10 @@
 package com.mstoyanov.myapplication.dao
 
 import android.content.Context
-import androidx.room.AutoMigration
 import androidx.room.Database
-import androidx.room.DeleteColumn
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import androidx.room.migration.AutoMigrationSpec
 import com.mstoyanov.myapplication.entity.Lesson
 import com.mstoyanov.myapplication.entity.LocalTimeConverter
 import com.mstoyanov.myapplication.entity.PhoneNumber
@@ -16,15 +13,12 @@ import com.mstoyanov.myapplication.entity.Student
 import com.mstoyanov.myapplication.entity.WeekdayConverter
 
 @Database(
-    version = 6,
+    version = 5,
     entities = [Student::class, PhoneNumber::class, Lesson::class],
-    autoMigrations = [AutoMigration(from = 4, to = 5, spec = MusicLessonsDatabase.MyAutoMigration::class)]
+    exportSchema = true
 )
 @TypeConverters(LocalTimeConverter::class, PhoneNumberTypeConverter::class, WeekdayConverter::class)
 abstract class MusicLessonsDatabase : RoomDatabase() {
-    @DeleteColumn(tableName = "student", columnName = "email")
-    class MyAutoMigration : AutoMigrationSpec
-
     abstract fun studentDao(): StudentDao
     abstract fun phoneNumberDao(): PhoneNumberDao
     abstract fun lessonDao(): LessonDao
